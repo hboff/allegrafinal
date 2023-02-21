44389c138cd9191a1c12c85d29ef972e7ed965cb
Oskar Lohse <oskar@baucampus.de>
2023-02-21 20:58:18 +0100

    routing 11

--- app/Http/Controllers/OrteController.php
+++ app/Http/Controllers/OrteController.php
@@ -39,7 +39,7 @@ class OrteController extends Controller
                  })
                  ->get();
         
-        $cityData = DB::table('orteDE')->select('laengengrad', 'breitengrad')->where('ort', $ort)->first();
+        $cityData = DB::table('orteDE')->select('laengengrad', 'breitengrad')->where('ort', $orteDE)->first();
         $laengengrad = $cityData->laengengrad;
         $breitengrad = $cityData->breitengrad;
 
