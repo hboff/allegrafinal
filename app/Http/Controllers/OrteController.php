b0022fdee414f0a3da3eb2e957b74d3d81de4347
Oskar Lohse <oskar@baucampus.de>
2023-02-21 21:16:28 +0100

    test 3

--- app/Http/Controllers/OrteController.php
+++ app/Http/Controllers/OrteController.php
@@ -12,31 +12,8 @@ class OrteController extends Controller
 {
     // Show single lisitng
     public function show($orteDE) {
-            
-        
-        $cityData = DB::table('orteDE')->select('laengengrad', 'breitengrad')->where('ort', $orteDE)->first();
-        $laengengrad = $cityData->laengengrad;
-        $breitengrad = $cityData->breitengrad;
-
-        $nearestCities = DB::select(DB::raw("
-            SELECT ort, (
-                3959 * acos (
-                    cos ( radians(?) )
-                    * cos( radians( breitengrad ) )
-                    * cos( radians( laengengrad ) - radians(?) )
-                    + sin ( radians(?) )
-                    * sin( radians( breitengrad ) )
-                )
-            ) AS distance
-            FROM orteDE
-            HAVING distance < 50
-            ORDER BY distance
-            LIMIT 0 , 16
-        "), [$breitengrad, $laengengrad, $breitengrad]);
-
-      
+       
         return view('immobilienbewertung', [
-            'nearestCities' => $nearestCities,
             'ortsname'=> $orteDE,
             ]);    }     
         
